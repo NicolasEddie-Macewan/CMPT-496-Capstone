c6a3edd36e8c5225d43a6315820d39fce1ea237e
NicolasEddie-Macewan <eddien@mymacewan.ca>
2026-02-16 17:19:44 -0700

    Update to output structure and summary node
    - Updated structured output for file summaries to include default empty values for optional fields
    - Partially implemented summary node

--- agent/nodes/summary_node.py
+++ agent/nodes/summary_node.py
@@ -1,5 +1,27 @@
 from agent.states.state1 import GraphState
 from agent.structured_output.summary_output import SummaryOutput
 
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3-flash-preview",
+    api_key=os.getenv("GOOGLE_API_KEY"))
+
 def summary_node(state: GraphState) -> SummaryOutput:
-    return state['summary']
+    """
+    """
+    file = state['files'].pop()
+    with open(file, 'r') as f:
+        contents = f.read()
+
+    structured_llm = llm.with_structured_output(SummaryOutput)
+    messages = [
+        ("system", "You are a helpful assistant that summarizes code."),
+        ("user", f"Summarize the following code file: \n\n{contents}")
+    ]
+
+    response = structured_llm.invoke(messages)
